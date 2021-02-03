92339bddf905cba91ad9eed3bbb28ef8d7664307
Chanasit <kai.chr160@gmail.com>
2021-02-03 14:32:59 +0700

    update px to em

--- widgets/health-check/index.jsx
+++ widgets/health-check/index.jsx
@@ -11,24 +11,22 @@ export const URL = styled("td")`
 `;
 
 export const Status = styled("td")`
-  color: ${(props) =>
-    props.status === "online" ? "rgb(153, 227, 160)" : "rgb(244, 115, 94)"};
+  color: ${(props) => props.status === "online" ? "rgb(153, 227, 160)" : "rgb(244, 115, 94)"};
 `;
 
 export const className = `
-	left: 10px;
-	top: 10px;
-	font-family: SFNS Display, 'Andale Mono', sans-serif;
-	text-shadow: 1px 1px 0 rgba(#000, 0.2);
+  margin: 1em;
+	font-family: "Hack Nerd Font";
+	text-shadow: .5em .5em 0 rgba(#000, 0.2);
 	background-color: rgba(0, 0, 0, 0.1);
-  font-size: 10px;
-  padding: 4px;
-	border-radius: 6px;
+  font-size: .6em;
+  padding: 1em;
+	border-radius: .8em;
 `;
 
 export const refreshFrequency = 60000; // widget will run command once a second
 
-export const command = " widgets/health-check/loop_list.sh  widgets/health-check/urls.txt";
+export const command = "widgets/health-check/loop_list.sh  widgets/health-check/urls.txt";
 
 export const initialState = { output: "http://localhost:80|offline" }
 
